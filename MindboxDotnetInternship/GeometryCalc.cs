709e6a9ec3bde47b0808afa287fd406dfaee8e6b
stas <@ohshie>
2024-09-05 18:18:56 +0200

    Two main requested methods dome

--- MindboxDotnetInternship/GeometryCalc.cs
+++ MindboxDotnetInternship/GeometryCalc.cs
@@ -12,8 +12,49 @@
 
 public class GeometryCalc
 {
-    public double GetSquareArea()
+    /// <summary>
+    /// Calculates Triangle Area from provided sides lengths. All lengths should be equal or more than 0.
+    /// Sum of 2 of any sides should be more or equal than third side.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <param name="c"></param>
+    /// <returns>Triangle Area</returns>
+    /// <exception cref="ArgumentException"></exception>
+    public double GetTriangleArea(double a, double b, double c)
     {
+        if (a < 0 || b < 0 || c < 0)
+        {
+            throw new ArgumentException("Triangle side should not be less than 0");
+        }
+
+        if (a + b <= c || b+c <= a || c+a <=b)
+        {
+            throw new ArgumentException("Sum of two triangle sides should not be less or equal to the third side");
+        }
         
+        var semiperimeter = (a+b+c) / 2;
+
+        var answer = Math.Sqrt(semiperimeter * (semiperimeter - a)* (semiperimeter - b) * (semiperimeter - c));
+        
+        return answer;
+    }
+
+    /// <summary>
+    /// Calculates Circle Area from provided Circle radius.
+    /// Radius should not be less than 0
+    /// </summary>
+    /// <param name="radius"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    public double GetCircleArea(double radius)
+    {
+        if (radius < 0)
+        {
+            throw new ArgumentException("Radius should not be less than 0");
+        }
+        var answer = Math.PI * Math.Pow(radius, 2);
+
+        return answer;
     }
 }
--- Tests/GeometryCalcTest.cs
+++ Tests/GeometryCalcTest.cs
@@ -5,14 +5,72 @@ namespace Tests;
 public class GeometryCalcTest
 {
     [Fact]
-    public void GetSquareAreaReturnsZero()
+    public void GetTriangleArea()
     {
-        var expectedAnswer = 0d;
+        // Короче, это решение нифига не клевое, но альтернативы выходят за рамки тестового
+        // все решения что я вижу предлагают экстеншен/хелпер писать для сравнения.
+        
+        var expectedAnswer = 43.301270189221931;
+
+        var geoCalc = new GeometryCalc();
+
+        var triangleArea = geoCalc.GetTriangleArea(10,10,10);
+        
+        Assert.Equal(expectedAnswer, triangleArea);
+    }
+
+    [Fact]
+    public void GetTriangleAreaLessThanZeroReturnsException()
+    {
+        var geoCalc = new GeometryCalc();
+
+        // подсмотрел на литкоде, прикольно, но очень криво. Создаю метод внутри метода, как-то ощущается неправильно
+        void ExceptionAction() => geoCalc.GetTriangleArea(-1, 0, 0);
+
+        Assert.Throws<ArgumentException>(ExceptionAction);
+    }
+
+    [Fact]
+    public void GetTriangleAreaTwoSidesLessThanThird()
+    {
+        var geoCalc = new GeometryCalc();
+
+        void ExceptionAction() => geoCalc.GetTriangleArea(1, 1, 0);
+
+        Assert.Throws<ArgumentException>(ExceptionAction);
+    }
+
+    [Fact]
+    public void GetCircleArea()
+    {
+        var expectedAnswer = Math.PI;
+
+        var geoCalc = new GeometryCalc();
+
+        var circleArea = geoCalc.GetCircleArea(1);
+        
+        Assert.Equal(expectedAnswer, circleArea);
+    }
+    
+    [Fact]
+    public void GetCirclAreaZeroRadius()
+    {
+        var expectedAnswer = 0;
         
         var geoCalc = new GeometryCalc();
 
-        var squareArea = geoCalc.GetSquareArea();
+        var circleArea = geoCalc.GetCircleArea(0);
         
-        Assert.Equal(squareArea, expectedAnswer);
+        Assert.Equal(expectedAnswer, circleArea);
+    }
+
+    [Fact]
+    public void GetCirclAreaNegativeRadius()
+    {
+        var geoCalc = new GeometryCalc();
+
+        void ExceptionAction() => geoCalc.GetCircleArea(-1);
+
+        Assert.Throws<ArgumentException>(ExceptionAction);
     }
 }
